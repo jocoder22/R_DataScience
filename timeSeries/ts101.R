f4e8a82967773306062e465174cd407936072d96
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-09-11 21:30:50 -0400

    Plot the diff(log)

--- timeSeries/ts101.R
+++ timeSeries/ts101.R
@@ -24,11 +24,15 @@ plot(diff(dow_jones))
 plot(log(dow_jones))
 plot(diff(log(dow_jones)))
 
+
+# plot the diff(1)
 dow_diff <- diff(dow_jones)
 dow_diff <- na.omit(dow_diff)
 acf(dow_diff)
 pacf(dow_diff)
 
+
+# plot the diff(log(x))
 dow_difflog <- na.omit(diff(log(dow_jones)))
 acf(dow_difflog)
 pacf(dow_difflog)
