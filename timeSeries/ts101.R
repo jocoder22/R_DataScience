0eb181cd372f837ee6b1272ef089a0b3df275634
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-09-12 21:50:42 -0400

    Update

--- timeSeries/ts101.R
+++ timeSeries/ts101.R
@@ -2,28 +2,27 @@
 options("getSymbols.warning4.0"=FALSE)
 
 # load library
-library("quantmod")
 library("zoo")
 library("xts")
+library("quantmod")
+
 
 
 # download Dow Jones data
 getSymbols("^DJI", src="yahoo")
-
-
 dow_jones <- DJI[, "DJI.Close"]
 
 
 # Plot the DJI close
 plot(dow_jones)
-par(mfrow=c(2,1))
+par(mfrow=c(2, 1))
 acf(dow_jones)
 pacf(dow_jones)
 
 
 # plot the detrending transformations
+par(mfrow=c(3, 1))
 plot(diff(dow_jones))
-par(mfrow=c(2,1))
 plot(log(dow_jones))
 plot(diff(log(dow_jones)))
 
@@ -31,7 +30,7 @@ plot(diff(log(dow_jones)))
 # plot the diff(1)
 dow_diff <- diff(dow_jones)
 dow_diff <- na.omit(dow_diff)
-par(mfrow=c(2,1))
+par(mfrow=c(2, 1))
 acf(dow_diff)
 pacf(dow_diff)
 
@@ -39,6 +38,6 @@ pacf(dow_diff)
 
 # plot the diff(log(x))
 dow_difflog <- na.omit(diff(log(dow_jones)))
-par(mfrow=c(2,1))
+par(mfrow=c(2, 1))
 acf(dow_difflog)
 pacf(dow_difflog)
