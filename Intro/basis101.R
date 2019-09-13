cabba9510e92f41f2ed77dacd2344dbe37475c52
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-09-13 19:57:37 -0400

    Add matrix multiplication

--- Intro/basis101.R
+++ Intro/basis101.R
@@ -10,3 +10,13 @@ X
 # Element by element multiplication
 X*X
 
+
+# Inner product X'X, 
+# t() is the transpose function
+# %*% denotes matrix multiplication
+X_prime_X <- t(X)%*%X
+X_prime_X
+
+# for XX'
+XX_prime <- X%*%t(X)
+XX_prime
