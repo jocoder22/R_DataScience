d122cb3be0f2889b8304008643ac66c2fddfe040
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-09-13 21:30:12 -0400

    Update

--- Intro/basis101.R
+++ Intro/basis101.R
@@ -1,9 +1,13 @@
+
+# install.packages("matlib", dependencies=TRUE, repos='http://cran.rstudio.com/')
 library(stats)
+library(matlib)
 a <- rnorm(100)
 b <- rnorm(100)
+c <- rnorm(100)
 
 # create a matrix
-X = cbind(a, b)
+X = cbind(a, b, c)
 X
 
 
@@ -17,6 +21,14 @@ X*X
 X_prime_X <- t(X)%*%X
 X_prime_X
 
+
 # for XX'
 XX_prime <- X%*%t(X)
 XX_prime
+
+
+# find the determinant
+det(X_prime_X)
+inverse <- inv(X_prime_X)
+inverse%*%X_prime_X
+solve(X_prime_X)
