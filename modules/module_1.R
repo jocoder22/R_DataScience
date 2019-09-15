730e81813364d0acdcc286225243d6c8b38e9c82
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-09-15 16:58:13 -0400

    Update

--- modules/module_1.R
+++ modules/module_1.R
@@ -8,9 +8,8 @@ library(quantmod)
 
 # Import dataset
 FinData <- read_excel("~/WorldQuant/MScFE 610 Econometrics/Datasets/WQU_Econometrics_Module1_Data.xlsx", 
-                        +     col_types = c("date", "numeric", "numeric", 
-                                            + "numeric", "numeric", "numeric", 
-                                                + "numeric", "numeric", "numeric"))
+                        col_types = c("date", "numeric", "numeric", "numeric", 
+                                      "numeric", "numeric", "numeric", "numeric", "numeric"))
 
 # view the dataset
 View(FinData)
@@ -34,6 +33,31 @@ dailyReturn(FinData$MSFT,  subset=NULL, type='arithmetic',leading=TRUE)
 x <- xts(FinData[,-1], FinData[,1])
 periodReturn(x,period='daily')
 
-Return.calculate(FinData$MSFT)
+Return.calculate(xts(FinData$MSFT), method="discrete")
 
 FinData$returns2 <- diff(FinData$MSFT)/lag(FinData$MSFT)
+
+
+
+
+
+
+
+require(quantmod)
+getSymbols("IBM", from = "1999-01-01", to = "2007-01-01")
+prices <- IBM[, "IBM.Close"]
+Returns <- diff(prices)/lag(prices)
+Returns <- Returns[-1]
+
+
+
+# }
+# NOT RUN {
+
+# }
+# NOT RUN {
+R.IBM = Return.calculate(prices, method="discrete")
+colnames(R.IBM)="IBM"
+chart.CumReturns(R.IBM,legend.loc="topleft", main="Cumulative Daily Returns for IBM")
+round(R.IBM,2)
+# }
