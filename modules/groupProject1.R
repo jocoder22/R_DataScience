c7bca4bf4c4dd12bead47415c85c03bc4cdf2394
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-09-15 16:56:27 -0400

    Add linear regression

--- modules/groupProject1.R
+++ modules/groupProject1.R
@@ -1,13 +1,39 @@
-install.packages("PerformanceAnalytics")
 
+library(xlsx)
 library(TTR)
 library(zoo)
 library(xts)
 library(quantmod)
 library(PerformanceAnalytics)
+library(tidyverse)
+library(stats)
+library(readxl)
+library(tseries)
+library(forcast)
 
 getSymbols("JPM", src="yahoo", from = "2018-02-01", to = "2018-12-30")
 JP_AdjClose <- JPM[, "JPM.Adjusted", drop = FALSE]
+
+getSymbols("^GSPC", src="yahoo", from = "2018-02-01", to = "2018-12-30")
+SP500_AdjClose <- GSPC[, "GSPC.Adjusted", drop = FALSE]
+
+allData <- data.frame(JP_AdjClose, SP500_AdjClose)
+
+# visualize the datasets
+head(JP_AdjClose)
+head(SP500_AdjClose)
+head(allData)
+
+
+# Save dataset for future analysis
+# Save dataset to excel
+
+# write.xlsx(JP_AdjClose,  file = "myworkbook.xlsx", sheetName = "Sheet1", 
+#            col.names = TRUE, row.names = TRUE, append = FALSE)
+# 
+# write.xlsx(allData,  file = "myworkbook2.xlsx", sheetName = "Sheet1", 
+#            col.names = TRUE, row.names = TRUE, append = FALSE)
+
 
 # Calculate basic statistics
 mean(JP_AdjClose)
@@ -18,8 +44,8 @@ StdDev(JP_AdjClose)
 
 # calculate daily stock return
 JPM_return <- CalculateReturns(JP_AdjClose)[-1]
-JP_AdjClose$Returns <- diff(JP_AdjClose)/lag(JP_AdjClose)
-JP_AdjClose <- JP_AdjClose[-1]
+JP_CloseReturns <- diff(JP_AdjClose)/lag(JP_AdjClose)
+
 
 
 # Plot the graph
@@ -28,3 +54,15 @@ plot(JP_AdjClose, type = "l", col = "blue", lwd = 2,
 
 plot(JPM_return, type = "l", col = "blue", lwd = 2, 
      ylab = "Daily Returns", main = "Daily stock return of JP Morgan")
+
+
+# Implement a two-variable regression
+fit <- lm(JPM.Adjusted ~ GSPC.Adjusted, data=allData)
+summary(fit)
+
+
+
+
+
+
+
