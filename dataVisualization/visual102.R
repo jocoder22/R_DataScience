a57474d48832ed3650dacac6bab0c79231f45f1e
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-09-17 05:48:43 -0400

    Add geom property for the whole dataset

--- dataVisualization/visual102.R
+++ dataVisualization/visual102.R
@@ -7,3 +7,12 @@ graph_object <- ggplot(mtcars, aes(x=wt, y=mpg, color=cyl))
 # Add visual elements
 graph_object + geom_point()
 
+# Add linear model liness, per cyl
+graph_object + geom_point() +
+  geom_smooth(method="lm", se=FALSE)
+
+
+# lm for the whole dataset
+graph_object + geom_point() +
+  geom_smooth(method="lm", se=FALSE) +
+  geom_smooth(aes(group=1), method="lm", se=FALSE, linetype=2)
