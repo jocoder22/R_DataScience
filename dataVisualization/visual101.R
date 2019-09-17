32fef4ad987b7293d36d6f689aa340ad8069f7df
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-09-16 22:07:39 -0400

    plot the graphs

--- dataVisualization/visual101.R
+++ dataVisualization/visual101.R
@@ -1,2 +1,18 @@
+
+# load required library
 library(ggplot2)
+
+# Do basic exploration
 str(mtcars)
+typeof(mtcars)
+class(mtcars)
+
+# plot basic graphs
+# Cyclinder: not as a factor
+par(mfrow=c(2, 1))
+ggplot(mtcars, aes(x=cyl, y=mpg))+
+  geom_point()
+
+# Cyclinder: as a factor
+ggplot(mtcars, aes(x=factor(cyl), y=mpg))+
+  geom_point()
