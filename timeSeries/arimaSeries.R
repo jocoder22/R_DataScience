98b2c2d2af2d4be8ca47cf39ecb50b26ee560397
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-09-18 21:47:39 -0400

    plot the acf and pacf

--- timeSeries/arimaSeries.R
+++ timeSeries/arimaSeries.R
@@ -1,5 +1,6 @@
 # load requried library  
-library(ggplot2)
+
+library(tseries)
 
 # explore the time series dataset 
 start(sunspots) # shows the start time
@@ -18,10 +19,12 @@ cycle(sunspots)
 head(sunspots)
 class(sunspots)
 str(sunspots)
+length(sunspots)
 
 # Check the class and plot the dataset
 is.ts(sunspots)
 ts.plot(sunspots)
+
 
 # subset the dataset
 sunspot_trun <- sunspots[c(1759:1800)] 
@@ -33,3 +36,21 @@ class(sunspot_trun)
 # convert to ts object and plot the subset
 sunspot_trun <- ts(sunspot_trun, start=1759, frequency = 1)
 ts.plot(sunspot_trun)
+
+
+# the dataset have seasonal components
+# Remove the seasonal element using diff and lag
+without_seasons <- diff(sunspots)
+ts.plot(without_seasons)
+
+
+acf(sunspots, lag.max = 40)
+pacf(sunspots, lag.max = 40)
+
+acf(without_seasons)
+pacf(without_seasons)
+
+
+
+
+
