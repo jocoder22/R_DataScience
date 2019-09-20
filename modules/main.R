0db276c769f9d5b94ff1d73b16b72547065a5439
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-09-20 19:17:44 -0400

    Calulate simple and compounded stock returns

--- modules/main.R
+++ modules/main.R
@@ -29,4 +29,31 @@ average_value
 stock_volatility <- sd(JPMorgan)
 stock_volatility
 # [1] 4.56665
+
+
 # 1.3 Calculate Daily stock returns
+# 1.3.1 Daily simple returns
+simple_returns <- diff(JPMorgan)/lag(JPMorgan)[-1]
+names(simple_returns) <- "JPM.simpleReturns"
+head(simple_returns)
+
+#             JPM.simpleReturns
+# 2018-02-02      -0.022161369
+# 2018-02-05      -0.047952357
+# 2018-02-06       0.030422834
+# 2018-02-07       0.006779018
+# 2018-02-08      -0.044210207
+# 2018-02-09       0.020022238
+
+
+# 1.3.2 Daily continous returns
+cont_returns <- diff(log(JPMorgan))[-1]
+names(cont_returns) <- "JPM.contReturns"
+head(cont_returns)
+#            JPM.contReturns
+# 2018-02-02    -0.022410622
+# 2018-02-05    -0.049140201
+# 2018-02-06     0.029969236
+# 2018-02-07     0.006756144
+# 2018-02-08    -0.045217271
+# 2018-02-09     0.019824429
