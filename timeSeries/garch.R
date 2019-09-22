08faf06362d45860778225cb782c2dbd9fcb96a2
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-09-22 16:19:34 -0400

    Calculate various volatility values

--- timeSeries/garch.R
+++ timeSeries/garch.R
@@ -1,18 +1,58 @@
 # Load the required packages
 library(quantmod, quietly=T)
 library(PerformanceAnalytics, quietly = T)
-require(TTR)
+library(TTR, quietly = T)
+library(glue, quietly = T)
 
 # set my file path
 currentdir <- getwd()
 filepath = file.path(currentdir,"timeSeries", "garch.rds")
 
 # Download datasets
-startDate <- "2018-02-01"
+startDate <- "1989-02-01"
 endDate <- "2018-12-30"
 
 getSymbols("^GSPC", src="yahoo", from=startDate)
 
 sp500 <- GSPC$GSPC.Adjusted
+names(sp500) <- "PriceClose"
 
-head(sp500)
+sp500$Returns <- CalculateReturns(sp500$PriceClose)
+
+
+# Create series charts
+chartSeries(sp500$Returns,name="S&P 500 Adjusted Close Price $USD")
+
+
+# estimated volatility
+# using volatility() from TTR package
+# first form the ohlc object
+ohlc <- GSPC[,c("GSPC.Open", "GSPC.High", "GSPC.Low", "GSPC.Close")]
+vClose <- volatility(ohlc, calc="close")
+vClose0 <- volatility(ohlc, calc="close", mean0=TRUE)
+vGK <- volatility(ohlc, calc="garman")
+vParkinson <- volatility(ohlc, calc="parkinson")
+vRS <- volatility(ohlc, calc="rogers")
+vGKy <- volatility(ohlc, calc="gk.yz")
+vYZ <- volatility(ohlc, calc="yang.zhang")
+vMC <- chaikinVolatility(GSPC[,c("GSPC.High", "GSPC.Low")])
+
+vtable <- data.frame(vClose, vClose0, vGK, vParkinson, vRS, vGKy, vYZ, vMC)
+tail(vtable)
+
+
+par(mfrow=c(3, 1))
+for(val in c("vClose", "vClose0", "vGK")){
+  string="Volatility plot for {val}."
+  ts.plot(vtable[val], main=glue(string), ylab="Volatility")
+}
+
+
+# Volatility skewness is a similar measure to omega but using the second 
+# partial moment. It's the ratio of the upside variance compared to the 
+# downside variance. Variability skewness is the ratio of the upside risk 
+# compared to the downside risk.
+MAR = 0.005
+print(VolatilitySkewness(sp500[,2], MAR, stat="volatility")) 
+print(VolatilitySkewness(sp500[,2], MAR, stat="variability")) 
+
