daeef042fd4a7dc53e7f014208a9ae84eef39380
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-09-22 05:07:14 -0400

    setsymbollookup

--- packages_reviews/_quantmod.R
+++ packages_reviews/_quantmod.R
@@ -1,4 +1,7 @@
-library(quantmod)
+
+# Load the required packages
+library(quantmod, quietly=T)
+
 
 
 # Download datasets
@@ -7,5 +10,12 @@ endDate <- "2018-12-30"
 
 getSymbols("JPM", src="yahoo", from=startDate, to=endDate )
 getSymbols("^GSPC", src="yahoo", from=startDate, to=endDate)
-getSymbols("DEXUSEU",src="FRED")
-getSymbols("XPT/USD",src="Oanda")
+getSymbols("DEXUSEU", src="FRED")
+getSymbols("USD/CNH", src="oanda")
+getSymbols("XPT/USD", src="oanda")
+
+
+# set multiple dataset lookup  
+setSymbolLookup(YHOO="google", GOOG="yahoo", CHF/USD="oanda")
+setSymbolLookup(DEXUSJP="FRED", AMZN="yahoo", CSUSHPINSA="FRED")
+saveSymbolLookup(file=)
