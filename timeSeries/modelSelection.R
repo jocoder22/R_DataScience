8de70197903a4d73ba0a1e460aff8bf8bd9fa480
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-09-27 19:43:22 -0400

    Add selection function

--- timeSeries/modelSelection.R
+++ timeSeries/modelSelection.R
@@ -3,7 +3,17 @@
 library(quantmod, quietly = T)
 library(PerformanceAnalytics, quietly = T)
 library(rugarch, quietly = T)
-library(dplyr)
+library(dplyr, quietly = T)
+library(glue, quietly = T)
+library(forecast, quietly = T)
+
+
+library(quantmod, quietly = T)
+library(forecast, quietly = T)
+library(lmtest, quietly = T)
+library(xts, quietly = T)
+library(aTSA, quietly = T)
+library(glue, quietly = T)
 
 # download dataset
 getSymbols("NYT", src="yahoo")
@@ -67,3 +77,70 @@ plot(resid1, main="Residual Model 1")
 # check correlations
 acf(nytReturn, main="ACF NYT Daily Returns")
 acf(abs(resid1), main="ACF Residual Model 1")
+
+
+# Box test on absolute residuals 
+Box.test(abs(resid1), 22, type = "Ljung-Box")
+Box.test(abs(resid2), 22, type = "Ljung-Box")
+Box.test(abs(resid3), 22, type = "Ljung-Box")$p.value
+
+
+modelSelection <- function(m1, m2){
+  res1 <- residuals(m1);
+  res2 <- residuals(m2);
+  rms1 <- mean(res1^2); 
+  rms2 <- mean(res2^2);
+  
+  lik1 <- likelihood(m1);
+  lik2 <- likelihood(m2);
+  
+  n1 <- length(coef(m1));
+  n2 <- length(coef(m2));
+  
+  aic1 <- infocriteria(m1)["Akaike",];
+  aic2 <- infocriteria(m2)["Akaike",];
+
+  bic1 <- infocriteria(m1)["Bayes",];
+  bic2 <- infocriteria(m2)["Bayes",];
+  
+  m11 <- deparse(substitute(m1));
+  m22 <- deparse(substitute(m2));
+  
+  resid1 <- residuals(m1, standardize = TRUE)
+  resid2 <- residuals(m2, standardize = TRUE)
+  bmol1 <- Box.test(abs(resid1), 22, type = "Ljung-Box")$p.value;
+  bmol2 <- Box.test(abs(resid2), 22, type = "Ljung-Box")$p.value;
+  
+  cat(sprintf("Ljung Box p-value for %s is %.3f\n", m11, bmol1))
+  cat(sprintf("Ljung Box p-value for %s is %.3f\n", m22, bmol2))
+  cat("Based on RMSE: ")
+  if (rms1 > rms2){
+    cat(sprintf("%s with %s parameter is better\n", m22, n2));
+  }
+  else cat(sprintf("%s with %s parameter is better\n", m11, n1));
+  
+  
+  cat("Based on Likelihood: ")
+  if (lik1 > lik2){
+    cat(sprintf("%s with %s parameter is better\n", m11, n1));
+  }
+  else cat(sprintf("%s with %s parameter is better\n", m22, n2));
+ 
+  cat("Based on Akaike Criteria (AIC): ")
+  if (aic2 > aic1){
+    cat(sprintf("%s with %s parameter is better\n", m11, n1));
+  }
+  else cat(sprintf("%s with %s parameter is better\n", m22, n2));
+  
+  
+  cat("Based on Bayes Creteria (BIC): ")
+  if (bic2 > bic1){
+    cat(sprintf("%s with %s parameter is better\n", m11, n1));
+  }
+  else cat(sprintf("%s with %s parameter is better\n", m22, n2));
+  
+}
+
+
+modelSelection(model1, model2)
+
