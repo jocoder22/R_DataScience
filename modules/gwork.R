020cee25c55996a05c4d86e9841eaafed6e6b7f7
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-10-01 19:57:27 -0400

    Update

---
+++ modules/gwork.R
@@ -0,0 +1,22 @@
+#########################################################################################
+#########################################################################################
+########## Analysis plan ################################################################
+# Load necessary packages
+library(quantmod)
+
+
+
+filepath <- getwd()
+sink(file.path(filepath, "modules", "submission2.doc"),  
+     append=FALSE, split=FALSE, type = c("output", "message"))
+
+
+
+
+
+
+
+
+
+sink()
+
