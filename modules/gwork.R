ac112e2027ee201f48871c53371b43c7ca4ad06f
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-10-03 22:05:16 -0400

    Update

--- modules/gwork.R
+++ modules/gwork.R
@@ -15,7 +15,7 @@ library(forecast, quietly = T)
 
 
 filepath <- getwd()
-sink(file.path(filepath, "modules", "submission2.doc"),  
+sink(file.path(filepath, "modules", "submission22.doc"),  
      append=FALSE, split=FALSE, type = c("output", "message"))
 
 
@@ -24,7 +24,7 @@ sink(file.path(filepath, "modules", "submission2.doc"),
 # Frequency: Daily
 
 # Download datasets
-startDate <- "2010-01-01"
+startDate <- "2000-01-01"
 endDate <- "2019-09-27"
 
 
@@ -33,97 +33,111 @@ apple <- AAPL[, "AAPL.Adjusted"]
 
 
 # Calculate Apple returns
-appleR <- CalculateReturns(apple)[-1]
-colnames(appleR) <- "Returns"
+appleReturns <- CalculateReturns(apple)[-1]
+colnames(appleReturns) <- "Returns"
 
 
 # plot the graphs
 par(mfrow = c(2,1), mar = c(2,3,3,3), oma = c(1, 1, 1, 1))
 plot(apple, main="Apple stock Adjusted Close price")
-plot(appleR, main="Apple stock Returns")
+plot(appleReturns, main="Apple stock Returns")
 
-
+head(apple)
+tail(apple)
 # compute basic statistics
-returnMean <- mean(appleR)
-returnVolatility <- sd(appleR)
-returnSkewness <- skewness(appleR)
-returnKurtosis <- kurtosis(appleR)
+returnMean <- mean(appleReturns)
+returnVolatility <- sd(appleReturns)
+returnSkewness <- skewness(appleReturns)
+returnKurtosis <- kurtosis(appleReturns)
 
+returnMean 
+returnVolatility 
+returnSkewness 
+returnKurtosis 
+
+# 
 # plot acf and pacf of apple returns and squared returns
-acf2(appleR, main="Apple Stock Returns")
-acf2(appleR^2, main="Apple Stock Returns Squared")
+acf2(appleReturns, main="Apple Stock Returns")
+acf2(appleReturns^2, main=" Squared Apple Stock Returns")
 
+frequency(apple)
+Box.test(appleReturns, type="Ljung-Box", lag=12)
+Box.test(appleReturns^2, type="Ljung-Box", lag=12)
 
-
-# Visualized distribution of rolling window volatility
-rollVol <- rollapply(appleR, width = 22 , FUN = "sd.annualized")
-rollVol <- na.omit(rollVol)
-names(rollVol) <- "rollingVol"
-
-
-rollVol$longVol <- returnVolatility
-volplot <- plot(rollVol[, "rollingVol"], main="Apple Stock Returns Volatility")
-volplot <- addSeries(rollVol[, "longVol"], col="red")
-volplot
-
-
-# plot the residuals
-predError <- appleR - returnMean
-par(mfrow = c(2,1), mar = c(4,3,3,3), oma = c(1, 1, 1, 1))
-plot(abs(predError), main="Absolute Prediction Error")
-acf(abs(predError), main="ACF of Absolute Prediction Error")
-
-
-
-# plot the graphs
-par(mfrow = c(2,1), mar = c(2,3,3,3), oma = c(1, 1, 1, 1))
-plot(appleR, main="Apple stock Returns")
-plot(rollVol, main="Apple stock Returns: 1 Month rolling Volatility")
-
-
-# compute formal statistical test: Autocorrelation and ARCH test
-Box.test(appleR^2, type = "Ljung-Box", lag = 30)
-ArchTest(appleR)
-
-
-source(file.path(filepath, "modules","garchAuto.R"))
-
-# spy = getSymbols("SPY", auto.assign=FALSE) 
-# rets = ROC(Cl(spy), na.pad=FALSE)
-fit = garchAuto(appleR, cores=1, trace=TRUE)
-# spy = getSymbols("SPY", auto.assign=FALSE)
-# rets = ROC(Cl(spy), na.pad=FALSE)
-# fit = garchAuto(rets, cores=1, trace=TRUE)
-
-# Analyzing (5,2,1,1) with sged distribution done.Good model. AIC = -5.567287, forecast: 0.0024
-
-
-# model using rugarch::
-garchspec <- ugarchspec(mean.model = list(armaOrder=c(3,2)),
-                        variance.model = list(model="sGARCH", garchOrder=c(1,1)),
-                        distribution.model = "norm")
-
-model4 <- ugarchfit(spec=garchspec, data=appleR)
-summary(model4)
-model4
-
-
-stdret <- residuals(model4, standardize = TRUE)
-
-
-# plot the histograms
-chart.Histogram(stdret, methods = c("add.normal","add.density" ), 
-                colorset = c("gray","red","blue"), 
-                main="Model Predicted Residuals under Normal Distribution")
-
-
-chart.Histogram(appleR, methods = c("add.normal","add.density" ), 
-                colorset = c("gray","red","blue"), 
-                main="Apple Returns under Normal Distribution")
-
- 
+ArchTest(coredata(appleReturns))
+adf.test(appleReturns)
+forecast::adf(appleReturns)
+tseries::adf.test(appleReturns)
+auto.arima(appleReturns)
+#
+# # Visualized distribution of rolling window volatility
+# rollVol <- rollapply(appleReturns, width = 22 , FUN = "sd.annualized")
+# rollVol <- na.omit(rollVol)
+# names(rollVol) <- "rollingVol"
+#
+#
+# rollVol$longVol <- returnVolatility
+# volplot <- plot(rollVol[, "rollingVol"], main="Apple Stock Returns Volatility")
+# volplot <- addSeries(rollVol[, "longVol"], col="red")
+# volplot
+#
+#
+# # plot the residuals
+# predError <- appleReturns - returnMean
+# par(mfrow = c(2,1), mar = c(4,3,3,3), oma = c(1, 1, 1, 1))
+# plot(abs(predError), main="Absolute Prediction Error")
+# acf(abs(predError), main="ACF of Absolute Prediction Error")
+#
+#
+#
+# # plot the graphs
+# par(mfrow = c(2,1), mar = c(2,3,3,3), oma = c(1, 1, 1, 1))
+# plot(appleReturns, main="Apple stock Returns")
+# plot(rollVol, main="Apple stock Returns: 1 Month rolling Volatility")
+#
+#
+# # compute formal statistical test: Autocorrelation and ARCH test
+# Box.test(appleReturns^2, type = "Ljung-Box", lag = 30)
+# ArchTest(appleReturns)
+#
+#
+# source(file.path(filepath, "modules","garchAuto.R"))
+#
+# # spy = getSymbols("SPY", auto.assign=FALSE)
+# # rets = ROC(Cl(spy), na.pad=FALSE)
+# fit = garchAuto(appleReturns, cores=1, trace=TRUE)
+# # spy = getSymbols("SPY", auto.assign=FALSE)
+# # rets = ROC(Cl(spy), na.pad=FALSE)
+# # fit = garchAuto(rets, cores=1, trace=TRUE)
+#
+# # Analyzing (5,2,1,1) with sged distribution done.Good model. AIC = -5.567287, forecast: 0.0024
+#
+#
+# # model using rugarch::
+# garchspec <- ugarchspec(mean.model = list(armaOrder=c(3,2)),
+#                         variance.model = list(model="fGARCH", submodel="TGARCH"),
+#                         distribution.model = "snorm")
+#
+# model4 <- ugarchfit(spec=garchspec, data=appleReturns)
+# summary(model4)
+# model4
+#
+#
+#  stdret <- residuals(model4, standardize = TRUE)
+#
+#
+# # plot the histograms
+# chart.Histogram(stdret, methods = c("add.normal","add.density" ),
+#                 colorset = c("gray","red","blue"),
+#                 main="Model Predicted Residuals under Normal Distribution")
+#
+#
+# chart.Histogram(predError, methods = c("add.normal","add.density" ),
+#                 colorset = c("gray","red","blue"),
+#                 main="Apple Returns under Normal Distribution")
+#
+#
 sink()
 
 
 
- 
