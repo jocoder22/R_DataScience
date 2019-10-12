93f20139888f7e7d1de0ea77bbaacf4d6db8661c
jocoder22 <okigbookey@gmail.com>
2019-10-12 14:07:17 -0400

    Using different formats

--- packages_reviews/quandl.R
+++ packages_reviews/quandl.R
@@ -1,11 +1,43 @@
 library(Quandl)
 library(PerformanceAnalytics)
+library(timeSeries)
 
-data1 <- Quandl("FRED/GDP", type="ts")
-head(data1)
+startdate = "2009-01-01"
+enddate = "2018-12-31"
 
-data2 <- Quandl("WIKI/FB", type = "xts")
-head(data)
+# formas:  “raw”, “ts”, “zoo”, “xts”, “timeSeries”
+# Request data as ts 
+data_raw <- Quandl("FRED/GDP", start_date=startdate, end_date=enddate, type="raw")
+head(data_raw)
+
+data_ts <- Quandl("FRED/GDP", start_date=startdate, end_date=enddate, type="ts")
+head(data_ts)
+
+
+data_xts <- Quandl("FRED/GDP", start_date=startdate, end_date=enddate, type="xts")
+head(data_xts)
+
+data_zoo <- Quandl("FRED/GDP", start_date=startdate, end_date=enddate, type="zoo")
+head(data_zoo)
+
+
+data_timeSeries <- Quandl("FRED/GDP", start_date=startdate, end_date=enddate, type="timeSeries")
+head(data_timeSeries)
+
+
+
+Facebook <- Quandl("WIKI/FB", type = "xts")
+head(Facebook$`Adj. Close`)
+
+
+
+# Specify a particular column
+Facebook_AdjClose <- Quandl("WIKI/FB.11", type = "xts")
+head(Facebook_AdjClose)
+
+Quandl("FRED/GDP", start_date=startdate, end_date=enddate)
+
+
 
 plot(data2$`Adj. Close`)
 plot(data2$Open)
