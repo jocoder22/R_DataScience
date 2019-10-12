2c9a70b6959b177dc604bfcde7addd1b7edc0b09
jocoder22 <okigbookey@gmail.com>
2019-10-11 23:56:56 -0400

    Update

--- packages_reviews/quandl.R
+++ packages_reviews/quandl.R
@@ -1,7 +1,11 @@
 library(Quandl)
+library(PerformanceAnalytics)
 
 data1 <- Quandl("FRED/GDP", type="ts")
 head(data1)
 
-data2 <- Quandl("WIKI/FB", type = "xts", )
+data2 <- Quandl("WIKI/FB", type = "xts")
 head(data)
+
+plot(data2$`Adj. Close`)
+
