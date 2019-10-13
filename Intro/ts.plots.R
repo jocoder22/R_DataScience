789e9f0815d0abcc2668777060187ab569a79611
jocoder22 <okigbookey@gmail.com>
2019-10-13 08:28:32 -0400

    Update

--- Intro/ts.plots.R
+++ Intro/ts.plots.R
@@ -46,3 +46,34 @@ ts.plot(returns, col=4:7, ylab="Index Simple REturns", xlab="Year",
 legend("topleft", colnames(EuStockMarkets), col = 4:7,
        lty = 1, bty = "n")
 
+
+
+# Compute means and standard deviations
+# Compute means from log_returns
+colMeans(log_returns)
+
+# apply to calculate sample variance from log_returns
+apply(log_returns, MARGIN = 2, FUN = var)
+
+# apply to calculate standard deviation from log_returns
+apply(log_returns, MARGIN = 2, FUN = sd)
+
+# plot histogram of percent returns for each index
+par(mfrow = c(2,2))
+apply(log_returns, MARGIN = 2, FUN = hist, main = "", xlab = "Log Return")
+
+
+# Display normal quantile plots of percent returns for each index
+par(mfrow = c(2,2))
+for(col in colnames(log_returns)){
+
+  # apply(log_returns, MARGIN = 2, FUN = qqnorm)
+  # qqline(log_returns)
+  qqnorm(log_returns[,col], main = sprintf("Normal Q-Q Plot for %s", col))
+  qqline(log_returns[,col])
+
+  
+}
+
+par(mfrow=c(1,1))
+
