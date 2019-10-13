ad5998ce868a5518c83c40896fd6b65451152d43
jocoder22 <okigbookey@gmail.com>
2019-10-13 06:51:56 -0400

    Plot graphs

--- Intro/ts.plots.R
+++ Intro/ts.plots.R
@@ -23,10 +23,21 @@ ts.plot(log_returns, col=4:7, ylab="Index LOg REturns", xlab="Year",
         main="Log Returns of Major European stock indexes")
 ts.plot(returns, col=4:7, ylab="Index Simple REturns", xlab="Year",
         main="Simple Returns of Major European stock indexes")
-legend("topleft", colnames(EuStockMarkets), col = 4:7,
-       lty = 1, bty = "n")
+
 
 plot(log_returns, col="red", ylab="Index LOg REturns", xlab="Year",
         main="Log Returns of Major European stock indexes")
 
+
+# Plot graphs again
 par(mfrow=c(1,1))
+ts.plot(log_returns, col=4:7, ylab="Index LOg REturns", xlab="Year",
+        main="Log Returns of Major European stock indexes")
+legend("topleft", colnames(EuStockMarkets), col = 4:7,
+       lty = 1, bty = "n")
+
+ts.plot(returns, col=4:7, ylab="Index Simple REturns", xlab="Year",
+        main="Simple Returns of Major European stock indexes")
+legend("topleft", colnames(EuStockMarkets), col = 4:7,
+       lty = 1, bty = "n")
+
