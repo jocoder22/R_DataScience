f4ed223b11bb2662098bc5459cb6d71758a11df3
jocoder22 <okigbookey@gmail.com>
2019-10-15 22:03:27 -0400

    Plot series

--- portfolio/portfolio101.R
+++ portfolio/portfolio101.R
@@ -13,3 +13,7 @@ getSymbols(tickers,from='2007-01-03')
 names = c('AMZN','AAPL','MSFT',"SPY","AGG", "VNQ", "GSG","JPM", "GSPC")
 portfolio  <- Ad(merge(AMZN, AAPL, MSFT, SPY, AGG, VNQ, GSG, JPM, GSPC))
 colnames(portfolio) <- names
+
+summary(coredata(portfolio))
+plot(portfolio[,2:8])
+plot(portfolio[, c(1,9)])
