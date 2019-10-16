0b8c0a613a0bc7bffe33763724e3cc9792444eaf
jocoder22 <okigbookey@gmail.com>
2019-10-15 21:33:08 -0400

    Update

---
+++ portfolio/portfolio101.R
@@ -0,0 +1,15 @@
+# Load the required packages
+library(quantmod, quietly=T)
+library(PerformanceAnalytics, quietly = TRUE)
+
+
+
+# Download stocks
+tickers = c('AMZN','AAPL','MSFT',"SPY", "AGG", "VNQ", "GSG","JPM", "^GSPC")
+getSymbols(tickers,from='2007-01-03') 
+
+
+# Merge them together
+names = c('AMZN','AAPL','MSFT',"SPY","AGG", "VNQ", "GSG","JPM", "GSPC")
+portfolio  <- Ad(merge(AMZN, AAPL, MSFT, SPY, AGG, VNQ, GSG, JPM, GSPC))
+colnames(portfolio) <- names
