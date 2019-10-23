cd490d2fcf2593669a626e79e955589d728e19dd
jocoder22 <okigbookey@gmail.com>
2019-10-23 18:53:02 -0400

    Update

--- Parallel/parallel_101.R
+++ Parallel/parallel_101.R
@@ -3,3 +3,23 @@ library(parallel)
 
 # check the number of cores
 num_cores  <- detectCores(logical = FALSE)
+
+
+# create the cluster
+cll <- makeCluster(num_cores)
+
+
+stdev  <- function(series){
+  sd(series)
+}
+
+
+number_reps <- 4000
+n_number_reps <- 100000
+
+# calculate the standard deviation
+
+std <- clusterApply(cll, x = rep(n_number_reps, number_rep),
+                    fun = stdev)
+
+hist(std)
