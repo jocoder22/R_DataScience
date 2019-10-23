219211609ee11a8cda7a3bcfbad784b70068a737
jocoder22 <okigbookey@gmail.com>
2019-10-22 21:34:04 -0400

    Update

--- timeSeries/dynamic_forecast.R
+++ timeSeries/dynamic_forecast.R
@@ -14,20 +14,26 @@ Frequency = 260
 # dates <- seq(as.Date("2016-01-01"), length = 5, by = "days")
 # bday <- as.POSIXct("1899-05-08")
 # dates <- as.Date("2016-01-01") + 0:4
-
-eu  <- EuStockMarkets
+# Convert to zoo object
+eu  <- zoo(EuStockMarkets)
+autoplot(eu, facets = FALSE)
 autoplot(eu, facets = TRUE)
+autoplot(eu)
 
-
-
+index(eu)
 # convert to xts
-dates <- seq(as.Date("1991-05-10"), length = 1860, by = "days")
-eux  <- xts(eu, order.by = dates)
-
-
+# dates <- seq(as.Date("1991-05-10"), length = 1860, by = "days")
+dates <- as.Date("1991-05-10") + 1:1860
+eux  <- xts(eu,  order.by = dates)
+plot(eux[, "CAC"], main = "CAC Index")
+plot(eux, main = "European Index")
+plot(eux, multi.panel = TRUE, main = "European Index")
 
 # fit the model with xreg, multivariate ARIMA
 # auto.arima(eu[, "DAX"], order = c(0,0,5), xreg = eu[, c(2:4)])
 model1  <- Arima(eu[, "DAX"], order = c(0,0,5), xreg = eu[, c(2:4)])
+
+
+
 
 
