4b6b543bd6533392cac8255760176082bfa83d31
jocoder22 <okigbookey@gmail.com>
2019-10-22 20:53:37 -0400

    Fit multivariate dynamic ARIMA

--- timeSeries/dynamic_forecast.R
+++ timeSeries/dynamic_forecast.R
@@ -1,13 +1,33 @@
 library(forecast)
 library(tseries)
+library(rugarch)
+library(xts)
 
 
 # Using the EuStockMarkets datasets
 # plot the series
-eu  <- EuStockMarkets
+Start = c(1991, 130) 
+End = c(1998, 169) 
+Frequency = 260 
 
+
+# dates <- seq(as.Date("2016-01-01"), length = 5, by = "days")
+# bday <- as.POSIXct("1899-05-08")
+# dates <- as.Date("2016-01-01") + 0:4
+
+eu  <- EuStockMarkets
 autoplot(eu, facets = TRUE)
 
 
-# fit the model with xreg
-model1  <- auto.arima(eu[, "DAX"], xreg = eu[, c(2:4)], stationary = TRUE)
+
+# convert to xts
+dates <- seq(as.Date("1991-05-10"), length = 1860, by = "days")
+eux  <- xts(eu, order.by = dates)
+
+
+
+# fit the model with xreg, multivariate ARIMA
+# auto.arima(eu[, "DAX"], order = c(0,0,5), xreg = eu[, c(2:4)])
+model1  <- Arima(eu[, "DAX"], order = c(0,0,5), xreg = eu[, c(2:4)])
+
+
