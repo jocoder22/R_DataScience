a14b1f07a600589d1ea5c4e7914f798df51fe701
jocoder22 <okigbookey@gmail.com>
2019-10-25 22:13:32 -0400

    Add new file mandelbrot.R

---
+++ dataVisualization/mandelbrot.R
@@ -0,0 +1,24 @@
+library(caTools)
+myjet.colors <-  colorRampPalette(c("black", "green","red", "blue", "#007FFF", "cyan", "#7FFF7F",
+                                    "yellow", "#FF7F00", "red", "#7F0000"))
+
+xdx <- 4300
+ydy <- 2400
+
+xc <- complex(real = rep(seq(-2.6, 1.5, length.out = xdx), each = ydy),
+              imag = rep(seq(-1.9, 1.3, length.out = ydy), xdx))
+
+
+
+
+xc <- matrix(xc, ydy, xdx)
+zz <- 0
+xxx <- array(0, c(ydy, xdx, 40))
+for(nk in 1:40){
+  
+  zz <-  zz^2 + xc
+  xxx[, , nk] <- exp(-abs(zz))
+  
+}
+
+write.gif(xxx, "MyMandelbrot.git", col = myjet.colors, delay = 110)
