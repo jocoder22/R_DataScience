b0367762a42c5b8d3c5ec96a39f4a6adab473063
jocoder22 <okigbookey@gmail.com>
2019-10-27 09:00:06 -0400

    Update

--- Parallel/parallel_102.R
+++ Parallel/parallel_102.R
@@ -4,18 +4,31 @@ library(microbenchmark)
 
 set.seed(150)
 
+outside <-  "Outside Variable"
+
+printt <-  function(){print(outside)}
+printt <-  function(){print(inside)}
 # check the number of cores
 num_cores  <- detectCores(logical = TRUE)  # ---> 8 cores
 # num_cores  <- detectCores(logical = FALSE) # ---> 4 cores
 
 
 # create the cluster
-cll <- makeCluster(num_cores)
+cll <- makeCluster(num_cores, type = "PSOCK")
+
+inside <-  "Inside Variable"
 
 str(cll)
 
 # get cluster IDs
 clusterCall(cll, Sys.getpid)
 
+
+clusterCall(cll, printt)
+
 # Stop the cluster
 stopCluster(cll)
+
+
+
+
