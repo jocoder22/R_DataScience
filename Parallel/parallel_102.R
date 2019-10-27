e4b6997653a00801ba716480c5a0aeafd084b317
jocoder22 <okigbookey@gmail.com>
2019-10-27 08:47:18 -0400

    Get cluster IDs

--- Parallel/parallel_102.R
+++ Parallel/parallel_102.R
@@ -2,4 +2,20 @@ library(parallel)
 library(microbenchmark)
 
 
-set.seed(10)
+set.seed(150)
+
+# check the number of cores
+num_cores  <- detectCores(logical = TRUE)  # ---> 8 cores
+# num_cores  <- detectCores(logical = FALSE) # ---> 4 cores
+
+
+# create the cluster
+cll <- makeCluster(num_cores)
+
+str(cll)
+
+# get cluster IDs
+clusterCall(cll, Sys.getpid)
+
+# Stop the cluster
+stopCluster(cll)
