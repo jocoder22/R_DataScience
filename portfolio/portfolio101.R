14bd0470b89843720f345a06ff8ab15cfff4985b
jocoder22 <okigbookey@gmail.com>
2019-10-28 18:31:43 -0400

    Add new file

--- portfolio/portfolio101.R
+++ portfolio/portfolio101.R
@@ -23,30 +23,30 @@ plot(portfolio[, c(1,9)])
 
 
 # Computer returns
-portfolioReturn  <- returns0(portfolio, method = "simple")
-portfolioReturn  <- xts(portfolioReturn, order.by = time(portfolio))
-
-r  <- rnorm(500)
-
+# portfolioReturn  <- returns0(portfolio, method = "simple")
+# portfolioReturn  <- xts(portfolioReturn, order.by = time(portfolio))
+# 
+# r  <- rnorm(500)
+portfolioReturn <- Return.calculate(portfolio)[-1]
 
 # create a model for the optimization
 # Create the portfolio specification
-model <- portfolio.spec(colnames(portfolioReturn[,3:8]))
+model <- PortfolioAnalytics::portfolio.spec(colnames(portfolioReturn[,3:8]))
 
 # Add a full investment constraint such that the weights sum to 1
-model <- add.constraint(portfolio = model, type = "full_investment")
+model <- PortfolioAnalytics::add.constraint(portfolio = model, type = "full_investment")
 
 # Add a long only constraint such that the weight of an asset is between 0 and 1
-model <- add.constraint(portfolio = model, type = "long_only")
+model <- PortfolioAnalytics::add.constraint(portfolio = model, type = "long_only")
 
 # Add an objective to maximize portfolio returns
-model <- add.objective(portfolio = model, type = "return", name = "mean")
+model <- PortfolioAnalytics::add.objective(portfolio = model, type = "return", name = "mean")
 
 # Add an objective to minimize portfolio standard deviation
-model <- add.objective(portfolio = model, type = "risk", name = "StdDev")
+model <- PortfolioAnalytics::add.objective(portfolio = model, type = "risk", name = "StdDev")
 
 # Solve the optimization problem
-optModel <- optimize.portfolio(portfolioReturn[,3:8], portfolio = model, trace = TRUE, 
+optModel <- PortfolioAnalytics::optimize.portfolio(portfolioReturn[,3:8], portfolio = model, trace = TRUE, 
                                optimize_method= "ROI", rp = r)
 
 
