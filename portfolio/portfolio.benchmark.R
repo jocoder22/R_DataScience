65370635387b4c3215a5b1a8daacd15ab5a91f82
jocoder22 <okigbookey@gmail.com>
2019-10-28 19:10:03 -0400

    Update

--- portfolio/portfolio.benchmark.R
+++ portfolio/portfolio.benchmark.R
@@ -17,9 +17,38 @@ names = c('AMZN','AAPL','MSFT',"SPY","AGG", "VNQ", "GSG","JPM", "GSPC")
 portfolio  <- Ad(merge(AMZN, AAPL, MSFT, SPY, AGG, VNQ, GSG, JPM, GSPC))
 colnames(portfolio) <- names
 
+
+# compute return of assets
+portfolioReturn <- Return.calculate(portfolio)[-1]
 num <- ncol(portfolio)
 
+
+# create equal weights
 weight_eq <- rep(1/num, num)
 
-benchmark <- Return.portfolio(R = )
+benchmark <- Return.portfolio(R = portfolioReturn,
+                              weights = weight_eq, rebalance_on = "years")
 
+colnames(benchmark) <- "benchmark"
+
+
+# Benchmark PerformanceAnalytics
+table.AnnualizedReturns(benchmark)
+
+
+
+# Constant weight
+constant_weight <- Return.portfolio(R = portfolioReturn,
+                                    weights = weight_eq, verbose = TRUE)
+
+
+rebalancing_weight <- Return.portfolio(R = portfolioReturn, verbose = TRUE,
+                                       weights = weight_eq, rebalance_on = "months")
+
+
+# create End of periods weights
+eop_cont <- constant_weight$EOP.Weight
+eop_rebal <- rebalancing_weight$EOP.Weight
+
+# plot the weights
+plot(eop_cont, main = "Plot of Portfolio Returns with Constant Weights")
