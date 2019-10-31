2d65d3c8057d94a01553cd326d2fca98da4488cc
jocoder22 <okigbookey@gmail.com>
2019-10-31 18:38:44 -0400

    Add new file portfolioPerformance.R

---
+++ portfolio/portfolioPerformance.R
@@ -0,0 +1,8 @@
+# Load the required packages
+library(quantmod, quietly = TRUE)
+library(PerformanceAnalytics, quietly = TRUE)
+library(ROI, quietly = TRUE)
+library(timeSeries, quietly = TRUE)
+library(tseries, quietly = TRUE)
+library(xts, quietly = TRUE)
+library(PortfolioAnalytics, quietly = TRUE))
