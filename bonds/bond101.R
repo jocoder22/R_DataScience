01cb08a6cfdb70e75cd1b8cc0d57b0b702dd2a11
jocoder22 <okigbookey@gmail.com>
2019-11-02 11:18:10 -0400

    Add new file bond101.R

---
+++ bonds/bond101.R
@@ -0,0 +1,37 @@
+library(Quandl)
+library(xts)
+
+
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+source("bondFunction.R")
+
+bondprice(1100, 0.051, 10, 0.07)
+
+
+# Obtain Moody's Baa  and Aaa index data
+startdate = "2003-01-01"
+baa <- Quandl("FED/RIMLPBAAR_N_M", start_date=startdate)
+aaa <- Quandl("FED/RIMLPAAAR_N_M", start_date=startdate)
+
+
+
+# Form xts object
+rating <- as.xts(data.frame(aaa$Value, baa$Value), order.by = aaa$Date) / 100
+colnames(rating) <- c("Aaa", "Baa")
+
+# plot the rating yields
+plot(rating, legend.loc = "topright", main = "Aaa rated Bond Yield vs Baa rated Bond Yield")
+
+
+# compute rating difference
+rating$Diff <- rating$Aaa - rating$Baa
+
+plot(rating$Diff, main = " Difference in Yield: Aaa rated Bond vs Baa rated Bond")
+
+# Identify 2018-06-24 yield
+yield20180624 <- subset(rating, index(rating) == "2015-05-31")
+
+yield20180624
+
+
