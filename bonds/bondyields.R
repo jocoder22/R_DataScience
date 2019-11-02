ad1a3f20a51df2d66827dd5e1ff01545c7c41c64
jocoder22 <okigbookey@gmail.com>
2019-11-02 14:58:21 -0400

    Add new file bondyields.R

---
+++ bonds/bondyields.R
@@ -0,0 +1,41 @@
+
+# setwd("~/R_DataScience/bonds")
+source("bondFunction.R")
+
+# Value bond using 5% yield
+bondprice(p = 100, cr = 0.05, ttm = 5, y = 0.05)
+
+# Value bond using 7% yield
+bondprice(p = 100, cr = 0.05, ttm = 5, y = 0.07)
+
+# Value bond using 6% yield
+
+bondprice(p = 100, cr = 0.05, ttm = 5, y = 0.06)
+
+
+
+bondyield(price = 98.79, p = 200, cr = 0.05, ttm = 15)
+bondyield(price = 95.79, p = 100, cr = 0.05, ttm = 5)
+bondyield(price = 95.79, p = 100, cr = 0.05, ttm = 10)
+bondyield(price = 95.79, p = 100, cr = 0.05, ttm = 3)
+
+
+# Calculate PV01 of a 10% bond
+# You can calculate the PV01 by calculating the value of a bond and the value of the 
+# same bond with a one basis point change in yield
+# Remember that PV01 is equivalent to the value of one bond minus the value of the other bond.
+
+PV01 <- abs(bondprice(p = 100, cr = 0.05, ttm = 5, y = 0.1001)-
+              bondprice(p = 100, cr = 0.05, ttm = 5, y = 0.10))
+PV01
+
+
+# Duration of a zero-coupon bond
+# Duration can sometimes be thought of as the weighted-average time to maturity 
+# of the bond. Because of interim cash flows, the duration of a coupon bond is less 
+# than its time to maturity. Based on that reasoning, what is the duration of a 
+# zero-coupon bond with three years to maturity? Three
+
+
+
+
