81b3444c626d60ed522422517cc359af254e4eee
jocoder22 <okigbookey@gmail.com>
2019-11-02 09:06:49 -0400

    Add new file bondFunction.R

---
+++ bonds/bondFunction.R
@@ -0,0 +1,25 @@
+
+
+bondprice <- function(p, cr, ttm, y){
+  
+  # create a vector of coupon payment layout
+  cp <- c(rep(p * cr, ttm - 1), p * (1 + cr))
+  
+  # create sequence of time horizon
+  tim <-  seq(1, ttm)
+  
+  # create a series of time multiplier
+  fac <-  1 / (1 + y)^tim
+  
+  # compute series of present value per year
+  psv <- cp * fac
+  
+  # sum the yearly present value 
+  sum(psv)
+  
+}
+
+
+bondprice(1100, 0.051, 10, 0.07)
+
+
